4b5a87c3ef10fe6e79afa127902275cabf0a5048
NithishGokul <125335843+NithishGokul@users.noreply.github.com>
2024-08-07 16:05:16 +0530

    Add files via upload

---
+++ day 9/service/TaskService.java
@@ -0,0 +1,32 @@
+package com.example.taskmanager.backend.service;
+
+import com.example.taskmanager.backend.model.Task;
+import com.example.taskmanager.backend.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
